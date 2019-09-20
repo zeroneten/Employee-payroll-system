7ec79e1b36e2c7e99ab93f94d97bd1fec94a593f
zerooneten <zerooneten018@gmail.com>
2019-09-20 17:00:46 +0300

    first commit

---
+++ src/Dbconnection.java
@@ -0,0 +1,31 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ndirituedwin
+ */
+public class Dbconnection {
+    Connection conn=null;
+    public static Connection Dbconnect(){
+         try{
+             Class.forName("com.mysql.jdbc.Driver");
+            Connection conn=DriverManager.getConnection("jdbc:mysql://localhost/organiization","root","");
+//            JOptionPane.showMessageDialog(null,"connected");
+            return conn;
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null,ex);
+            return null;
+        }
+    
+}
+    
+}
